18708d743afe026bc0f926e2e74c1d07d01fc996
Pedro William <68715618+pewilliam@users.noreply.github.com>
2022-06-16 21:14:53 -0300

    Adicionado reposta da questão 2

---
+++ Questões/App2/App2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0;
+            int n2 = 1;
+            int n3 = 0;
+
+            List<int> list = new List<int>();
+
+            Console.Write("Digite a quantidade de índices: ");
+            int qtd = int.Parse(Console.ReadLine());
+            Console.Write("Digite o número que deve ser checado: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < qtd; i++)
+            {
+                Console.Write(n1 + " ");
+                list.Add(n1);
+
+                n3 = n1 + n2;
+                n1 = n2;
+                n2 = n3;
+            }
+
+            Console.WriteLine();
+
+            if (list.Contains(n))
+            {
+                Console.WriteLine("O número " + n + " existe na sequência!");
+            }
+            else
+            {
+                Console.WriteLine("O número " + n + " não existe na sequência.");
+            }
+        }
+    }
+}
